22f590dcf3cd93c601f8ea51c0ff088b2f9b588d
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-12-10 16:19:41 +0100

    Init Project

---
+++ front/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  watch: {
+    usePolling: true
+  },
+  resolve: {
+    alias: {
+      '@root': '/src',
+      '@assets': '/src/assets',
+      '@styles': '/src/styles',
+      '@components': '/src/components',
+      '@datas': '/src/datas',
+    },
+  },
+})
