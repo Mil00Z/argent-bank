c7c2b8c2197ab7ff773adc3c2809bbba356633ba
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-12-11 18:07:04 +0100

    NotFound Page

---
+++ front/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import {Link} from 'react-router'
+
+const NotFound = () => {
+
+  return (  
+
+    <section className="not-found-container">
+        <h1 className="title">404</h1>
+        <p className="description">Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/" className="link">Retourner sur la page d'accueil</Link>
+      </section>  )
+}
+export default NotFound
