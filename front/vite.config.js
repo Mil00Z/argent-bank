2c2b4156610d52ef2b1600f51247701bfb9d128b
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-12-11 12:54:15 +0100

    Install react router + alias for Vite Bundle

--- front/vite.config.js
+++ front/vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
       '@assets': '/src/assets',
       '@styles': '/src/styles',
       '@components': '/src/components',
+      '@pages': '/src/pages',
       '@datas': '/src/datas',
     },
   },
