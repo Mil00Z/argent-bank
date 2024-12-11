6ec68cdbafb8ba2a7ab5e8466d239d3c12ee99b2
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-12-11 18:09:39 +0100

    Create Page Component

--- front/src/pages/Root.jsx
+++ front/src/pages/Root.jsx
@@ -1,17 +1,16 @@
 import {Outlet} from 'react-router'
 
-import SignIn from '@pages/SignIn'
-import Home from '@pages/Home'
-import User from '@pages/User'
+import Navigation from '@components/navigation/navigation'
+import Footer from '@components/footer/footer'
 
-import '@styles/App.css'
-import '@styles/main.css'
 
 function Root () {
  
   return (
     <>
-      <Outlet />
+      <Navigation layout={'connect'} />
+        <Outlet />
+      <Footer />
     </>
   )
 }
