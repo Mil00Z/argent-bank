af773b6cf96fc8b113ad7c5a1754a8b018adf862
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-12-17 17:18:05 +0100

    create Logic with login functions, testing inputs and store datas

---
+++ front/src/components/form/form.jsx
@@ -0,0 +1,126 @@
+import { useState } from "react";
+import {Link,useOutletContext} from 'react-router';
+
+
+const FormSignin = () => {
+
+  const {user} = useOutletContext();
+
+  const [inputUserEmail,setInputUserEmail] = useState('');
+  const [inputUserPass,setInputUserPass] = useState('');
+  
+  // const [userLogin, setUserLogin] = useState({})
+  
+  const triggerForm = (e) => {
+
+    //Not refresh
+    e.preventDefault();
+
+    let datas = {
+      email: inputUserEmail,
+      password: inputUserPass
+    }
+
+    //Ask to API if the access is OK
+    login('http://localhost:3001/api/v1/user/login',datas);
+
+
+     //Get local values in inputs
+     stockDataInputs('#signin',datas);
+
+
+  };
+
+
+  const getUserEmail = (value) => {
+    
+    let emailRegex = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
+
+    if (value.length > 6 && emailRegex.test(value)) {
+
+      setInputUserEmail(value);
+    
+    }
+
+  }
+
+  const getUserPass = (value) => {
+
+    let regExTest = new RegExp("^[a-zA-Z0-9_-]+$");
+
+    if(value.length > 6 && regExTest.test(value)) {
+
+      setInputUserPass(value);
+
+    } 
+
+}
+
+
+  function stockDataInputs(target,datas) {
+
+    // let form = document.querySelector(`${target}`);
+
+    // let formDatas = new FormData(form);
+
+    // Stock Credits in LocalStorage
+    localStorage.setItem(`user-${user.firstName}`, JSON.stringify(datas));
+
+  }
+
+  function checkUserStorage() {
+
+    let checkUser = JSON.parse(localStorage.getItem(`user-${inputUsername}`)) || false;
+
+      return checkUser
+
+  }
+
+  async function login(url,settings) {
+
+    let params = {
+      method:"post",
+      body: JSON.stringify(settings),
+      headers : {
+        "Content-Type":"application/json"
+      }
+    }
+
+    try {
+
+      const response = await fetch(url,params);
+      const datas = await response.json();
+
+      console.log(datas);
+
+      return datas
+
+  } catch (error) {
+
+      console.warn(error);
+
+    }
+    
+  }
+
+ 
+
+  return(
+    <form id="signin" onSubmit={triggerForm}>
+      <div className="input-wrapper">
+            <label htmlFor="email">Username</label>
+            <input type="email" id="email" name="email" onChange={(e) => getUserEmail(e.target.value)} />
+      </div>
+      <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password"  onChange={(e) =>getUserPass(e.target.value)} />
+      </div>
+      <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+      </div>
+      <button className="sign-in-button">Sign In</button>
+    </form>
+  )
+}
+export default FormSignin
