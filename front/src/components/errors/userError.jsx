3d2446d94df1f06a1981e4b24f6d7235c7ef38ae
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-12-18 18:39:25 +0100

    Create Component Error User Profile

---
+++ front/src/components/errors/userError.jsx
@@ -0,0 +1,12 @@
+const UserError = () => {
+
+  return(
+    
+    <section className="user-error-container">
+        <p className="error debeug">Erreure de chargement du profil Utilisateur</p>
+    </section>
+  
+)
+
+}
+export default UserError
