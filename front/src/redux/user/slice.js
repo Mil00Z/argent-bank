071b91950099d472dc0390f7dde9e63223a85e81
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-01-20 18:33:21 +0100

    update name of payload in userSlice

--- front/src/redux/user/slice.js
+++ front/src/redux/user/slice.js
@@ -1,1 +1,15 @@
-export const userSlice = '' 
+import {createSlice} from '@reduxjs/toolkit'
+
+
+const initialiasedState = {userCredits:null}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState:initialiasedState,
+  reducers : {
+    setUser : (state,action) => {
+      return {...state,userCredits:action.payload}
+    },
+  }
+})
+
