38bd07f5a0b21ba3cef2cf05c0fbd5168f2dd6a9
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-01-21 16:49:53 +0100

    create local State to display UserEdit Component

--- front/src/pages/User.jsx
+++ front/src/pages/User.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect} from 'react'
+import { useState,useEffect, Component} from 'react'
 import {useNavigate} from 'react-router'
 
 import { useSelector,useDispatch} from 'react-redux';
@@ -7,17 +7,22 @@ import { useSelector,useDispatch} from 'react-redux';
 import { userSlice } from '../redux/user/slice';
 import { useGetUserMutation } from '../redux/user/api';
 
+// UI Component
+import UserEdit from '@components/userEdit/userEdit';
 import UserError from '@components/errors/userError'
 
 
 import '@styles/pages/_User.scss'
 
 
-
-
 const User = () => {
 
   const [loged,setLoged] = useState(false);
+
+  const [isEdit,setIsEdit] = useState(false);
+
+  const [animate,setAnimate] = useState('');
+
 
   const token = useSelector(state => state.auth?.token);
 
@@ -26,6 +31,8 @@ const User = () => {
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
+
+
 
   // Hook from RTK Mutation
   const [getUser,{data,error,isLoading}] = useGetUserMutation();
@@ -105,47 +112,36 @@ const User = () => {
   }
 
 
+  function triggerEditUser(){
 
-   
-  // useEffect(() => {
-
-  //   //Limiter la portée + Syntaxe standard avec useEffect (car async n'est pas possible sur UseEffect)
-  //   async function checkUserStorage(){
-
-
-  //     if(localStorage.getItem('user-token')){
-  
-  //       navigate('/login');
-         
-  //     } else {
-  
-  //       await checkUser('http://localhost:3001/api/v1/user/profile',token);
-
-  //       setLoading(false);
-  
-  //     }
-
-  //   }
-
-  //   checkUserStorage()
     
+      setIsEdit(isEdit => true);
 
-  // }, []);
+      setAnimate(animate => true);
+  }
 
 
+
+//Scenarii
 
 if(isLoading) return <h2>Loading...
   <p>{ token ? token : 'No Token'}</p> 
 </h2>
  
+
 if(!loged) return <UserError />;
+
+
 
   return(
       <>
         <div className="header">
           <h1 className="main-title">Welcome back<br />{user.firstName} {user.lastName}</h1>
-          <button className="edit-button" onClick={() => {console.log('Edit Name')}}>Edit Name</button>
+          <button className="edit-button" onClick={() => {triggerEditUser()}}>Edit Name</button>
         </div>
+        
+        {isEdit ? <UserEdit fadeIn={animate} /> : null}
+
         <h2 className="sr-only">Accounts</h2>
         <section className="account" data-user={user.id}>
           <div className="account-content-wrapper">
@@ -177,6 +173,7 @@ if(!loged) return <UserError />;
             <button className="transaction-button">View transactions</button>
           </div>
         </section>
+
     </>
       
   )
