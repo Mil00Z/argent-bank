ca17f4028868844085d0f91a802ae65396ae7c02
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-01-22 17:46:39 +0100

    quick fix beug user ?

--- front/src/pages/User.jsx
+++ front/src/pages/User.jsx
@@ -139,7 +139,7 @@ if(!loged) return <UserError />;
         {isEdit ? <UserEdit fadeIn={animate} /> : null}
 
         <h2 className="sr-only">Accounts</h2>
-        <section className="account" data-user={user.id}>
+        <section className="account" data-user={user?.id}>
           <div className="account-content-wrapper">
             <h3 className="account-title">Argent Bank Checking (x8349)</h3>
             <p className="account-amount">$2,082.79</p>
